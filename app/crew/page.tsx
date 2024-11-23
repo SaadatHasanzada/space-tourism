5f89166c47a57b0e4440a1a2c66e7c7f4cd5f3ba
Seadet <seadet.hesenzade2000@gmail.com>
2024-11-24 02:02:23 +0400

    done

--- app/crew/page.tsx
+++ app/crew/page.tsx
@@ -1,5 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import data from "@/data.json";
+import style from "./style.module.scss";
+import { useState } from "react";
+
 const CrewPage = () => {
-  return <div>page</div>;
+  const crews = data.crew;
+  const [crew, setCrew] = useState(crews[0]);
+
+  return (
+    <div className={`container ${style.crew}`}>
+      <h1 className={style.title}>
+        <span>02</span>MEET YOUR CREW
+      </h1>
+      <div className={style.crew_info}>
+        <div className={style.crew_container}>
+          <div className={style.crew_details}>
+            <p className={style.crew_role}>{crew.role}</p>
+            <h2>{crew.name}</h2>
+            <p className={style.crew_bio}>{crew.bio}</p>
+          </div>
+          <div className={style.crew_menu}>
+            {crews.map((crewMember) => (
+              <button
+                key={crewMember.name}
+                onClick={() => setCrew(crewMember)}
+                className={
+                  crewMember.name === crew.name ? `${style.active}` : ""
+                }
+              ></button>
+            ))}
+          </div>
+        </div>
+        <div className={style.crew_image}>
+          <Image
+            src={crew.images.webp}
+            alt={crew.name}
+            width={500}
+            height={676}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CrewPage;
--- app/technology/page.tsx
+++ app/technology/page.tsx
@@ -1,5 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+import data from "@/data.json";
+import style from "./style.module.scss";
+
 const TechnologyPage = () => {
-  return <div>page</div>;
+  const technologies = data.technology;
+  const [selectedTech, setSelectedTech] = useState(technologies[0]);
+  const [isLandscape, setIsLandscape] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLandscape(window.innerWidth >= 768 && window.innerWidth <= 1024);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  return (
+    <div className={`container ${style.technology}`}>
+      <h1 className={style.title}>
+        <span>03</span>SPACE LAUNCH 101
+      </h1>
+      <div className={style.tech_info}>
+        <div className={style.tech_container}>
+          <div className={style.tech_menu}>
+            {technologies.map((tech, index) => (
+              <button
+                key={tech.name}
+                onClick={() => setSelectedTech(tech)}
+                className={
+                  tech.name === selectedTech.name ? `${style.active}` : ""
+                }
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
+          <div className={style.tech_details}>
+            <p className={style.text}>THE TERMINOLOGY…</p>
+            <h2>{selectedTech.name}</h2>
+            <p className={style.tech_desc}>{selectedTech.description}</p>
+          </div>
+        </div>
+        <div className={style.tech_image}>
+          <Image
+            src={
+              isLandscape
+                ? selectedTech.images.landscape
+                : selectedTech.images.portrait
+            }
+            alt={selectedTech.name}
+            width={isLandscape ? 768 : 608}
+            height={isLandscape ? 357 : 600}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TechnologyPage;
